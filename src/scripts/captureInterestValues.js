36c2bf017530cc781ee75281d88805bd3b5dd1f0
Sam Cronin <croninsw@gmail.com>
2019-02-23 15:22:33 -0600

    Didn't get edit to work before time was up.

--- src/scripts/captureInterestValues.js
+++ src/scripts/captureInterestValues.js
@@ -3,8 +3,7 @@ const interestObject = (interest) => {
     <section id="interestObject">
         <h2>${interest.name}</h2>
         <div>${interest.description}</div>
-       <button id="edit--${interest.id}">Edit Interest</button>
-       <button id="delete--${interest.id}">Delete Interest</button>
+        <button id="delete--${interest.id}">Delete Interest</button>
     </section>
     `
 }
